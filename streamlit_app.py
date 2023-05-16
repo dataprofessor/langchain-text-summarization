fd7d447fad9cdb54275d2d0ec663ba1dac1e7bab
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-16 22:09:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,26 @@
 import streamlit as st
-import openai
 import os
-import requests
+import openai
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
 
 # Set OpenAI API key as environment variable
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 
-# Initialize session state
-if 'txt' not in st.session_state:
-    st.session_state['txt'] = ''
+# Instantiate the LLM model
+llm = OpenAI(temperature=0)
 
-    
+# Split text
+text_splitter = CharacterTextSplitter()
+texts = text_splitter.split_text(txt)
+
+# Create multiple documents
+docs = [Document(page_content=t) for t in texts[:3]]
+
+# Text summarization
+chain = load_summarize_chain(llm, chain_type="map_reduce")
+chain.run(docs)
+
 st.title('Text Summarization with Langchain')
-
-txt = st.text_area('Text to analyze', '')
-
-st.write('Hello world!')
+txt = st.text_area('Enter your text', '')
