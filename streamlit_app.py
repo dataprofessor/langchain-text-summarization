55733b81e61990bb629714d4cd4125f31004a112
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-16 21:17:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import openai
+import os
+import requests
 
+# Set OpenAI API key as environment variable
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+
+# Initialize session state
+if 'txt' not in st.session_state:
+    st.session_state['txt'] = ''
+
+    
 st.title('Text Summarization with Langchain')
+
+txt = st.text_area('Text to analyze', '')
 
 st.write('Hello world!')
