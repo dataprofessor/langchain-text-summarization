0c8c031b8fa21bababf4e7e5f8f9ac703a2e04d9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-17 18:21:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,15 @@ from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 
-with st.sidebar:
-  # Check if OpenAI API key is in secrets
-  if 'OPENAI_API_KEY' in st.secrets:
-    st.success('Key is provided!', icon='🔑')
-    os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-  # If API key is not in secrets, ask user to enter key
-  else:
-    st.error('Please enter your API key!', icon='⚠️')
-    api_key = st.text_input('Enter OpenAI API key:', type='password')
-    os.environ['OPENAI_API_KEY'] = api_key
+# Check if OpenAI API key is in secrets
+if 'OPENAI_API_KEY' in st.secrets:
+  st.sidebar.success('Key is provided!', icon='🔑')
+  os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+# If API key is not in secrets, ask user to enter key
+else:
+  st.error('Please enter your API key!', icon='⚠️')
+  api_key = st.text_input('Enter OpenAI API key:', type='password')
+  os.environ['OPENAI_API_KEY'] = api_key
 
 def summarize(txt):
   # Instantiate the LLM model
