3210bd3703363d15e20d4af8518e740d999a35b9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-17 17:59:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from langchain.chains.summarize import load_summarize_chain
 
 def func():
   if st.secrets['OPENAI_API_KEY'] in locals():
-    return st.write('A)
+    return st.write('A')
   else:
     return st.write('B')
 func()
