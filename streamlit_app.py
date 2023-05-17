ef6a9fa65a7d31eb7bbf07b422656dab49e29e3d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-17 18:18:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,27 +5,16 @@ from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 
-# Check if OpenAI API key is in secrets
-if 'OPENAI_API_KEY' in st.secrets:
-  st.success('Key is provided!', icon='🔑')
-  os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-else:
-  st.error('Please enter your API key!', icon='⚠️')
-  api_key = st.text_input('Enter OpenAI API key:', type='password')
-  os.environ['OPENAI_API_KEY'] = api_key
-
-#api_key = st.text_input('Enter OpenAI API key:', type='password')
-#if api_key:
-#  os.environ['OPENAI_API_KEY'] = api_key  
-  
-#if os.getenv('OPENAI_API_KEY'):
-  # Set OpenAI API key as environment variable
-  #os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-  #os.environ['OPENAI_API_KEY'] = st.text_input('Enter OpenAI API key:', type='password')
-  #st.success('Key is provided!', icon='🔑')
-#else:
-  #st.error('Please enter API key!', icon='⚠️')
-
+with st.sidebar:
+  # Check if OpenAI API key is in secrets
+  if 'OPENAI_API_KEY' in st.secrets:
+    st.success('Key is provided!', icon='🔑')
+    os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+  # If API key is not in secrets, ask user to enter key
+  else:
+    st.error('Please enter your API key!', icon='⚠️')
+    api_key = st.text_input('Enter OpenAI API key:', type='password')
+    os.environ['OPENAI_API_KEY'] = api_key
 
 def summarize(txt):
   # Instantiate the LLM model
@@ -39,8 +28,8 @@ def summarize(txt):
   chain = load_summarize_chain(llm, chain_type="map_reduce")
   return st.info(chain.run(docs))
 
+# Form to accept user's text input for summarization
 st.title('🦜🔗 Text Summarization App')
-
 with st.form("summarize_form"):
   txt_input = st.text_area('Enter your text', '', height=200)
   submitted = st.form_submit_button("Submit")
