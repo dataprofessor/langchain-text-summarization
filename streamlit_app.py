594411efbada51dee94a2732fa396ae6245ad435
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-07 13:24:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,10 @@ with st.form('summarize_form', clear_on_submit=True):
     openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
     submitted = st.form_submit_button('Submit')
     if submitted and openai_api_key.startswith('sk-'):
-        response = generate_response(txt_input)
-        result.append(response)
-        del openai_api_key
+        with st.spinner('Calculating...'):
+            response = generate_response(txt_input)
+            result.append(response)
+            del openai_api_key
 
 if len(result):
     st.info(response)
